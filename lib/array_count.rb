d491e20cc57db90a117cd2cde1d2bc0a14a67ef2
Joy Evans <joyevans@me.com>
2020-10-15 02:14:55 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -7,11 +7,7 @@ end
 def count_empty_strings(array)
   array.count do |element|
     
-    #array.all?(&:blank?)
-    #element.is_a? String
     element.to_s.empty?
-    #"".size == 0
-    #"".empty?
     
   end
 end
